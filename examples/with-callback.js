8824a044f82e3765e80fa834f920016862f89ade
Mitscherlich Wei <mitscherlich36+github@gmail.com>
2018-04-02 21:33:20 +0800

    📦 add seeta models

--- examples/with-callback.js
+++ examples/with-callback.js
@@ -1,14 +1,19 @@
 const seeta = require('..');
 
-const imagePath = __dirname + '/../data/image_0002.png';
-const modelPath = __dirname + '/../models/seeta_fd_frontal_v1.0.bin';
-
-let image;
+const imagePath = __dirname + '/../data/image_0001.png';
+const fdModelPath = __dirname + '/../models/seeta_fd_frontal_v1.0.bin';
+const faModelPath = __dirname + '/../models/seeta_fa_v1.1.bin';
+const frModelPath = __dirname + '/../models/seeta_fr_v1.0.bin';
 
 seeta.loadImage(imagePath, (err, destPath) => {
-  image = destPath;
 
-  seeta.detect(image, modelPath, (err, result) => {
+  seeta.detect(image, faModelPath, (err, result) => {
+    // result looks like '{ "faces": [ [ x, y, width, height ], ... ] }'
+    console.log(JSON.parse(result));
+  });
+
+  seeta.align(image, faModelPath, faModelPath, (err, result) => {
+    // result looks like '{ "faces": [ { "points": [ x, y ] }, ... }'
     console.log(JSON.parse(result));
   });
 });
